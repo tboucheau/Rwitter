ac19b07fa0e82353ce70a29e7c613e12310089c2
Tony Boucheau <boucheau.tony@neuf.fr>
2017-01-25 16:49:16 +0100

    update render edit if no updates done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,11 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user.update(user_params)
-        redirect_to tweets_path, notice: 'User was successfully updated.'
+        if @user.update(user_params)
+          redirect_to tweets_path, notice: 'User was successfully updated.'
+        else
+          render 'edit'
+        end
     end
 
     
