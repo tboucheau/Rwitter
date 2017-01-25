84a2a98029b81c2701e5e4bf7be6cb7bd1df01d0
Tony Boucheau <boucheau.tony@neuf.fr>
2017-01-25 06:27:32 +0100

    associations users tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tweets
 end
